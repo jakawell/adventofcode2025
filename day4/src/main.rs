ee629acc2706d2ceec2cebb009bf21c934af3f6e
Jesse Kawell <jesse.kawell@icf.com>
2025-12-04 10:38:41 -0500

    Day 4, Part 2 (I'm not proud of that main fn okay)

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,17 +3,34 @@ use utils::read_lines;
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
         let mut lines = lines.map_while(Result::ok);
-        println!("Answer: {}", process_input(&mut lines));
+        let mut total = 0;
+
+        let process_output = process_input(&mut lines);
+        let removals = process_output.0;
+        let mut updated_state = process_output.1.into_iter();
+        total += removals;
+
+        loop {
+            let process_output = process_input(&mut updated_state);
+            let removals = process_output.0;
+            updated_state = process_output.1.into_iter();
+            total += removals;
+            if removals < 1 {
+                break;
+            }
+        }
+        println!("Answer: {total}");
     } else {
         println!("Maybe missing the file.");
     }
 }
 
-pub fn process_input<T>(lines: &mut T) -> i32
+pub fn process_input<T>(lines: &mut T) -> (i32, Vec<String>)
 where
     T: Iterator<Item = String>,
 {
     let mut total = 0;
+    let mut updated_state: Vec<String> = Vec::new();
 
     let first_line = &lines.next().unwrap();
     let space_length = first_line.len();
@@ -25,7 +42,10 @@ where
 
     for next_row in lines {
         row_below = next_row;
-        total += check_row(row_above.as_bytes(), row.as_bytes(), row_below.as_bytes());
+        let (count, update_row) =
+            check_row(row_above.as_bytes(), row.as_bytes(), row_below.as_bytes());
+        total += count;
+        updated_state.push(update_row);
 
         // move up
         row_above = row;
@@ -34,18 +54,22 @@ where
 
     // check last row with another fictional empty row below it
     row_below = '.'.to_string().repeat(space_length);
-    total += check_row(row_above.as_bytes(), row.as_bytes(), row_below.as_bytes());
+    let (count, update_row) = check_row(row_above.as_bytes(), row.as_bytes(), row_below.as_bytes());
+    total += count;
+    updated_state.push(update_row);
 
-    total
+    (total, updated_state)
 }
 
-pub fn check_row(row_above: &[u8], row: &[u8], row_below: &[u8]) -> i32 {
+pub fn check_row(row_above: &[u8], row: &[u8], row_below: &[u8]) -> (i32, String) {
     let mut debug_printable: String = String::from("");
+    let mut updated_row: String = String::from("");
 
     let mut sum = 0;
     for (index, location) in row.iter().enumerate() {
         if *location != b'@' {
             debug_printable.push(*location as char);
+            updated_row.push(*location as char);
             continue;
         }
 
@@ -66,14 +90,16 @@ pub fn check_row(row_above: &[u8], row: &[u8], row_below: &[u8]) -> i32 {
         if local_sum < 4 {
             sum += 1;
             debug_printable.push('x');
+            updated_row.push('.')
         } else {
             debug_printable.push(*location as char);
+            updated_row.push(*location as char);
         }
     }
 
     println!("{debug_printable}");
 
-    sum
+    (sum, updated_row)
 }
 
 fn count_location(location: &u8) -> i32 {
@@ -91,6 +117,6 @@ mod tests {
         let row = "@@@.@.@.@@".as_bytes();
         let row_below = "@@@@@.@.@@".as_bytes();
 
-        assert_eq!(check_row(row_above, row, row_below), 1);
+        assert_eq!(check_row(row_above, row, row_below).0, 1);
     }
 }
