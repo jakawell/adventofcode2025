f1cfffa333b0e49dcc0b8d39a02ebb9253dba188
Jesse Kawell <jakawell@gmail.com>
2025-12-06 23:55:53 -0500

    Day 6, Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -35,53 +35,85 @@ where
     sum
 }
 
-pub fn process_input_part2<T>(lines: &mut T) -> i64
+#[derive(Debug)]
+struct ColumnInfo {
+    operator: char,
+    size: usize,
+    start_index: usize,
+    value_set: Vec<String>,
+}
+
+pub fn process_input_part2<T>(lines: &mut T) -> u64
 where
     T: Iterator<Item = String>,
 {
     let mut sum = 0;
 
-    let (rows, operators) = load_data(lines);
+    // this will store all the number records for later processing, which yes is putting the whole input in memory, but the whole file is 19kb so bite me
+    let mut data_rows: Vec<String> = Vec::new();
+    let mut columns: Vec<ColumnInfo> = Vec::new();
 
-    // a single "value set" is a set of numbers that a single operator will be applied to
-    let mut value_sets: Vec<Vec<String>> = vec![Vec::new(); operators.len()];
-    // we iterate over each row, updating the value set for each column
-    for row in rows {
-        for (value_set_index, number) in row.iter().enumerate() {
-            // we reverse the number and append each numeral to the associated value in the value set (the value at the numeral's index)
-            let reversed_number: String = number
-                .to_string()
-                .chars()
-                .rev()
-                .map(|i| i.to_string())
-                .collect::<String>();
-            dbg!(&reversed_number);
-            let value_set = &mut value_sets[value_set_index];
-
-            for (col_index, numeral) in reversed_number.chars().enumerate() {
-                if let Some(value) = value_set.get(col_index) {
-                    let mut value = value.clone();
-                    value.push(numeral);
-                    value_set[col_index] = value;
+    // first we find the last row to get information about the column sizes
+    for line in lines {
+        let mut line_chars = line.chars().enumerate();
+        let mut first_char = line_chars.next().unwrap().1;
+        let mut column_start_index = 0;
+        if first_char == '+' || first_char == '*' {
+            let mut column_size: usize = 0; // we start at zero because there will always be an extra whitespace character separating the columns
+            for (index, element) in line_chars {
+                if element.is_whitespace() {
+                    column_size += 1;
                 } else {
-                    value_set.push(numeral.to_string());
+                    columns.push(ColumnInfo {
+                        operator: first_char,
+                        size: column_size,
+                        start_index: column_start_index,
+                        value_set: vec![String::new(); column_size],
+                    });
+                    first_char = element;
+                    column_size = 0;
+                    column_start_index = index;
                 }
+            }
+            // add the last column
+            columns.push(ColumnInfo {
+                operator: first_char,
+                size: column_size + 1, // add one to correct for logic assuming an extra whitespace after each column
+                start_index: column_start_index,
+                value_set: vec![String::new(); column_size + 1],
+            });
+        } else {
+            data_rows.push(line.clone());
+        }
+    }
+
+    // next we iterate over each row to get the value sets to operate on
+    for row in data_rows.iter() {
+        let mut current_column_index: usize = 0;
+        let mut current_column = &mut columns[current_column_index];
+        for (index, element) in row.chars().enumerate() {
+            if index > current_column.start_index + current_column.size {
+                current_column_index += 1;
+                current_column = &mut columns[current_column_index];
+            }
+
+            if element.is_numeric() {
+                current_column.value_set[index - current_column.start_index].push(element);
             }
         }
     }
 
-    for (index, operator) in operators.iter().enumerate() {
-        let mut total = if *operator == "+" { 0i64 } else { 1i64 };
-        let value_set = &value_sets[index];
-        for value in value_set {
-            print!("{operator} {} ", value);
-            if *operator == "+" {
-                total += value.parse::<i64>().unwrap();
+    // finally we complete the operations
+    for column in columns {
+        let mut total = if column.operator == '+' { 0 } else { 1 };
+        for value in column.value_set {
+            let value: u64 = value.parse().unwrap();
+            if column.operator == '+' {
+                total += value;
             } else {
-                total *= value.parse::<i64>().unwrap();
+                total *= value;
             }
         }
-        println!("= {total}");
         sum += total;
     }
 
